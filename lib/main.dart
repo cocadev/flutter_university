689e0b35e573681dd181d0089ccabd0875ceaf4d
funnyjerry <wenhu95piao@gmail.com>
2019-05-24 14:56:53 +0800

    add Timer

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,79 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Timer'),
         ),
         body: Center(
-          child: Text('Hello Eugene'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomTextContainer(label: 'HRS'),
+                  CustomTextContainer(label: 'MIN'),
+                  CustomTextContainer(label: 'SEC'),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: RaisedButton(
+                  child: Text('START'),
+                  onPressed: () {
+
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+//class CustomTextContainer extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Text('00');
+//  }
+//}
+
+class CustomTextContainer extends StatelessWidget {
+  CustomTextContainer({this.label, this.value});
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      padding: EdgeInsets.all(20),
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10),
+        color: Colors.black87,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            '00',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '$label',
+            style: TextStyle(
+              color: Colors.white70,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
