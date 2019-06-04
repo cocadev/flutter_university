fe274559464ff9e096a88506f9607b852d0415e5
Jerry <40970351+funnyjerry@users.noreply.github.com>
2019-06-05 00:17:05 +0800

    add image and router finished

--- lib/router.dart
+++ lib/router.dart
@@ -1,215 +1,42 @@
 import 'package:flutter/material.dart';
-//import 'demo/second.dart';
-//import 'demo/image.dart';
-//import 'demo/timer.dart';
-//import 'demo/guide.dart';
-//import 'demo/inputs.dart';
-import 'twitter/main.dart';
-import 'whatsapp/main.dart';
-import 'TodoAppUI/main.dart';
-import 'FlightSearch/main.dart';
-import 'chess/main.dart';
-import 'easyPip/main.dart';
-import 'YouTube/main.dart';
-import 'Randomize/main.dart';
-import 'FlutterBoxBar/main.dart';
-import 'SimpleAnimation/main.dart';
-import 'ButterCMS/main.dart';
 
-class Router extends StatefulWidget {
-  @override
-  _RouterState createState() => _RouterState();
-}
+class HomePage extends StatelessWidget{
 
-class _RouterState extends State<Router> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: new EdgeInsets.all(15.0), // Or set whatever you want
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            new Container(
-              margin:
-                  const EdgeInsets.only(top: 12.0, bottom: 10.0, left: 10.0),
-              child: new Text(
-                "FunnyJerry Flutter",
-                style: new TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.pink,
-                  fontWeight: FontWeight.bold,
+    var widgetList = ["Text", "Appbar", "Container", "Column", "Row", "Button", "Stack", "Stack2", "Stack3", "Stack4" ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ListView Title"),
+      ),
+      body: ListView.builder(
+          itemCount: widgetList.length,
+          shrinkWrap: true,
+          itemBuilder: (builder, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+              child: InkWell(
+                onTap: (){
+                  Navigator.of(context).pushNamed('${widgetList[index]}');
+                },
+                child: Card(
+                  elevation: 2.0,
+                  child: new Container(
+                      color: Colors.white,
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.sentiment_very_satisfied ),
+                          Text(widgetList[index], style: TextStyle(color: Colors.black, fontSize: 20.0,),),
+                        ],
+                      )
+                  ),
                 ),
               ),
-            ),
-
-            new Container(
-                margin: const EdgeInsets.all(10.0),
-                child: Column(children: <Widget>[
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => ButterCMSApp()));
-                    },
-                    child: const Text('Goto ButterCMS',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => SimpleAnimationApp()));
-                    },
-                    child: const Text('Goto SimpleAnimation',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => BoxApp()));
-                    },
-                    child: const Text('Goto BoxBar',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Randomize()));
-                    },
-                    child: const Text('Goto Randomize',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => YoutubeApp()));
-                    },
-                    child: const Text('Goto YoutubeApp',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => EasyPipApp()));
-                    },
-                    child: const Text('Goto EasyPiP',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MyChessApp()));
-                    },
-                    child: const Text('Goto Chess Game',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FlightSearchApp()));
-                    },
-                    child: const Text('Goto FlightSearchApp',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => TodoApp()));
-                    },
-                    child: const Text('Goto TodoApp',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => WhatsApp()));
-                    },
-                    child: const Text('Goto WhatsApp',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                  RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => TwitterApp()));
-                    },
-                    child: const Text('Goto TwitterApp',
-                        style: TextStyle(fontSize: 20)),
-                    color: Colors.lightBlue,
-                    textColor: Colors.white,
-                  ),
-                ]))
-
-//            Center(
-//              child: RaisedButton(
-//                child: Text("TextInput"),
-//                onPressed: (){
-//                  Navigator.push(context, MaterialPageRoute(builder: (context) => Inputs()) );
-//                },
-//                color: Colors.amber,
-//              ),
-//            ),
-//            Center(
-//              child: RaisedButton(
-//                child: Text("Guide"),
-//                onPressed: (){
-//                  Navigator.push(context, MaterialPageRoute(builder: (context) => Guide(title:'Beginner Guide')) );
-//                },
-//                color: Colors.amber,
-//              ),
-//            ),
-//            Center(
-//              child: RaisedButton(
-//                child: Text("Navigate to page 2"),
-//                onPressed: (){
-//                  Navigator.push(context, MaterialPageRoute(builder: (context) => Second(title: "Página 2",)) );
-//                },
-//                color: Colors.amber,
-//              ),
-//            ),
-//            Center(
-//              child: RaisedButton(
-//                child: Text("Navigate to page image"),
-//                onPressed: (){
-//                  Navigator.push(context, MaterialPageRoute(builder: (context) => ImageScreen()) );
-//                },
-//                color: Colors.amber,
-//              ),
-//            ),
-//            Center(
-//              child: RaisedButton(
-//                child: Text("My Timer"),
-//                onPressed: (){
-//                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyTimerApp()) );
-//                },
-//                color: Colors.amber,
-//              ),
-//            ),
-          ],
-        ));
+            );
+          }),
+    );
   }
 }
